aa031f982f90d83b196efb11dfeb97a4eccee4a7
HashEngineering <hashengineeringsolutions@gmail.com>
2021-02-26 18:32:17 -0800

    Add other tests to test script

--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@
 
 ./tester.out tests/simple_test.txt tests/simple_output.txt
 ./tester.out tests/sample_input.txt tests/sample_output.txt
+./tester.out tests/complicated_input.txt
+./tester.out tests/some_tests.txt
+./tester.out tests/floats.txt
