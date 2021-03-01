7e509fe2ef77e5d07b120b4f431ac9e75e698c75
HashEngineering <hashengineeringsolutions@gmail.com>
2021-03-01 10:12:42 -0800

    Output more information to the screen about files used

--- tester.cpp
+++ tester.cpp
@@ -10,20 +10,27 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-  cout << "Tester" << endl << endl;
+  cout << "Tokenizer" << endl << endl;
   if (argc < 2) {
-    cerr << "Please provide an input file name and output test file. Exiting.\n";
+    cerr << "tester.out <input-file-name> [expected-output-file-name]" << endl << endl;
+    cerr << "Please provide an input file name and optional expected output file. Exiting.\n";
     exit(1);
   }
+
+  cout << "Processing file: " << argv[1] << endl << endl;
   string input_file_stream = argv[1];
   tokenizer tester(input_file_stream);
   string dot = ".";
   string file_handle = input_file_stream.substr(0, input_file_stream.find(dot));
   string extension = "_output.txt";
   file_handle = file_handle + extension;
+  cout << "Outputing results to the screen and to this file: " << file_handle << endl << endl;
+
   list<Record> records = tester.parse_input(file_handle);
   // output locatin is passed to parse_input
   if (argc > 2) {
+    cout << "comparing results to expected file: " << argv[2] << endl << endl;
+
     string expected_output_file = argv[2];
 
     tokenizer_checker checker(expected_output_file);
