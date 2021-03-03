74fdc0825e58c0b4a2eb42e9774129e0ed6eceed
Sean <seanjaviya@gmail.com>
2021-03-03 10:35:00 -0800

    Changed executable name to tokenizer, tested on Mac Big Sur

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ g++ -c -o tokenizer.o tokenizer.cpp -std=c++17 -g
 g++ -c -o tokenizer_checker.o tokenizer_checker.cpp -std=c++17 -g
 
 g++ -c -o lexicalscanner.o lexicalscanner.cpp -std=c++17
-g++ -o tester.out token.o tokenizer.o tester.o tokenizer_checker.o lexicalscanner.o -std=c++17 -g
+g++ -o tokenizer tokenizer.o tester.o tokenizer_checker.o lexicalscanner.o -std=c++17 -g
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./tester.out tests/simple.txt tests/simple_expected.txt
-./tester.out tests/sample.txt tests/sample_expected.txt
-./tester.out tests/quadratic.txt tests/quadratic_expected.txt
-./tester.out tests/countEights.txt tests/countEights_expected.txt
-./tester.out tests/floats.txt tests/floats_expected.txt
+./tokenizer tests/simple.txt tests/simple_expected.txt
+./tokenizer tests/sample.txt tests/sample_expected.txt
+./tokenizer tests/quadratic.txt tests/quadratic_expected.txt
+./tokenizer tests/countEights.txt tests/countEights_expected.txt
+./tokenizer tests/floats.txt tests/floats_expected.txt
